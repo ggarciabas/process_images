dbdf8f22125a4ee06cbc987211dac89f9337c75e
Giovanna Garcia Basilio <ggarciabas@MacBook-Air-de-Giovanna.local>
2019-08-12 18:18:14 -0300

    Organizando estrutura

--- full_process.sh
+++ full_process.sh
@@ -4,40 +4,34 @@
 # watermark
 # copyright
 
-folder=$1
-size=$2
-
-mkdir -p "./source/photo/"$size"/"
-
-for path in $folder"/"$size"/"*/
+for path in "./source/new/"*/
 do
     for path_image in "$path"*
-    do         
+    do       
+        echo $path_image  
         image=$(basename "$path_image")
         album=$(basename "$path")
         newalbum=$(echo $album | sed -e 's/[a-z]//g' | sed -e 's/ /-/' | sed -e 's/ //g')
-        echo $image        
-        mkdir -p "./source/photo/"$size"/$newalbum"
+        echo $image
+        mkdir -p "./source/photo/thumb/$newalbum"
 
-        cp $folder"/"$size"/"$newalbum"/""$image" "./source/photo/""$size""/""$newalbum""/""$image"
-
-        # autorotate
-        convert "./source/photo/""$size""/""$newalbum""/""$image" -auto-orient "./source/photo/""$size""/""$newalbum""/""$image"
+        # resize -> {width: 1920, height: 1080, crop: false, upscale: false}
+        convert "./source/new/""$newalbum""/""$image" -resize 200x200 -auto-orient -quality 70% "./source/photo/thumb/""$newalbum""/""$image"        
 
         # watermark
         lw=$(identify -format '%w' "./_logo/logo_branco.bmp")
         lh=$(identify -format '%h' "./_logo/logo_branco.bmp")      
-        ih=$(identify -format '%h' "./source/photo/""$size""/""$newalbum""/""$image")
+        ih=$(identify -format '%h' "./source/photo/thumb/""$newalbum""/""$image")
         ch=$(echo "0.1 * $ih" | bc -l)
         cw=$(echo "($ch * $lw)/$lh" | bc -l)
 
         # read
         convert "./_logo/logo_branco.bmp" -resize $(echo $cw)x$(echo $ch)\! logo_.bmp
         echo $cw"x"$ch
-        composite -gravity northwest logo_.bmp "./source/photo/""$size""/""$newalbum""/""$image" "./source/photo/""$size""/""$newalbum""/""$image"
-        composite -gravity southeast logo_.bmp "./source/photo/""$size""/""$newalbum""/""$image" "./source/photo/""$size""/""$newalbum""/""$image"
+        composite -gravity northwest logo_.bmp "./source/photo/thumb/""$newalbum""/""$image" "./source/photo/thumb/""$newalbum""/""$image"
+        composite -gravity southeast logo_.bmp "./source/photo/thumb/""$newalbum""/""$image" "./source/photo/thumb/""$newalbum""/""$image"
         
         # copyright
-        exiv2 -M"set Exif.Image.Copyright GGarciaBas All rights reserved 2019" "./source/photo/""$size""/""$newalbum""/""$image"
+        exiv2 -M"set Exif.Image.Copyright GGarciaBas All rights reserved 2019" "./source/photo/thumb/""$newalbum""/""$image"
     done
 done
